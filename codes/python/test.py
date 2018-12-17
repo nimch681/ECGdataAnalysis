0aeb1e70723019327bdba3b69b2dc9cfe0d34126
nimch681 <chontiran@gmail.com>
2018-12-17 22:11:40 +1300

    experimenting with a new segmentation method

--- codes/python/test.py
+++ codes/python/test.py
@@ -38,6 +38,9 @@ for i in range(0, len(beats)):
 findpeaks_limit = 0.35
 findpeaks_spacing = 72#360
 integration_window = 16
+refractory_period = 180
+#threshold_value = 0.0
+
 
 filtered_ecg_measurements = None
 differentiated_ecg_measurements = None
@@ -45,6 +48,10 @@ squared_ecg_measurements = None
 #integrated_ecg_measurements = None
 detected_peaks_indices = None #important
 detected_peaks_values = None # important
+
+
+
+MLII = mit100.filtered_MLII
 
 def detect_peaks(signal):
     """
@@ -111,40 +118,95 @@ def findpeaks(data, spacing=1, limit=None):
         ind = ind[data[ind] > limit]
     return ind
 
-def detect_qrs(self):
+def detect_qrs(signal,detected_peaks_indices, detected_peaks_values):
+    
+    qrs_peak_value = 0.0
+    noise_peak_value = 0.0
+    ecg_data_detected = np.array(signal)
+
+    qrs_peak_filtering_factor = 0.125
+    noise_peak_filtering_factor = 0.125
+    qrs_noise_diff_weight = 0.25
+    threshold_value = 0.0
+    qrs_peaks_indices = np.array([], dtype=int)
+    noise_peaks_indices = np.array([], dtype=int)
     """
     Method responsible for classifying detected ECG measurements peaks either as noise or as QRS complex (heart beat).
     """
-    for detected_peak_index, detected_peaks_value in zip(self.detected_peaks_indices, self.detected_peaks_values):
+    for detected_peak_index, detected_peaks_value in zip(detected_peaks_indices, detected_peaks_values):
 
         try:
-            last_qrs_index = self.qrs_peaks_indices[-1]
+            last_qrs_index = qrs_peaks_indices[-1]
         except IndexError:
             last_qrs_index = 0
 
         # After a valid QRS complex detection, there is a 200 ms refractory period before next one can be detected.
-        if detected_peak_index - last_qrs_index > self.refractory_period or not self.qrs_peaks_indices.size:
+        if detected_peak_index - last_qrs_index > refractory_period or not qrs_peaks_indices.size:
             # Peak must be classified either as a noise peak or a QRS peak.
             # To be classified as a QRS peak it must exceed dynamically set threshold value.
-            if detected_peaks_value > self.threshold_value:
-                self.qrs_peaks_indices = np.append(self.qrs_peaks_indices, detected_peak_index)
+            if detected_peaks_value > threshold_value:
+                qrs_peaks_indices = np.append(qrs_peaks_indices, detected_peak_index)
 
                 # Adjust QRS peak value used later for setting QRS-noise threshold.
-                self.qrs_peak_value = self.qrs_peak_filtering_factor * detected_peaks_value + \
-                                        (1 - self.qrs_peak_filtering_factor) * self.qrs_peak_value
+                qrs_peak_value = qrs_peak_filtering_factor * detected_peaks_value + \
+                                        (1 - qrs_peak_filtering_factor) * qrs_peak_value
             else:
-                self.noise_peaks_indices = np.append(self.noise_peaks_indices, detected_peak_index)
+                noise_peaks_indices = np.append(noise_peaks_indices, detected_peak_index)
 
                 # Adjust noise peak value used later for setting QRS-noise threshold.
-                self.noise_peak_value = self.noise_peak_filtering_factor * detected_peaks_value + \
-                                        (1 - self.noise_peak_filtering_factor) * self.noise_peak_value
+                noise_peak_value = noise_peak_filtering_factor * detected_peaks_value + \
+                                        (1 - noise_peak_filtering_factor) * noise_peak_value
 
             # Adjust QRS-noise threshold value based on previously detected QRS or noise peaks value.
-            self.threshold_value = self.noise_peak_value + \
-                                    self.qrs_noise_diff_weight * (self.qrs_peak_value - self.noise_peak_value)
+            threshold_value = noise_peak_value + \
+                                    qrs_noise_diff_weight * (qrs_peak_value - noise_peak_value)
 
     # Create array containing both input ECG measurements data and QRS detection indication column.
     # We mark QRS detection with '1' flag in 'qrs_detected' log column ('0' otherwise).
-    measurement_qrs_detection_flag = np.zeros([len(self.ecg_data), 1])
-    measurement_qrs_detection_flag[self.qrs_peaks_indices] = 1
-    self.ecg_data_detected = np.append(self.ecg_data, measurement_qrs_detection_flag)
+    measurement_qrs_detection_flag = np.zeros([len(signal), 1])
+    measurement_qrs_detection_flag[qrs_peaks_indices] = 1
+    ecg_data_detected = np.column_stack((signal, measurement_qrs_detection_flag))
+    return ecg_data_detected, qrs_peaks_indices 
+
+
+detected_peak_indices, detected_peak_values = detect_peaks(MLII)
+ecg_data_detected, qrs_peaks_indices = detect_qrs(MLII,detected_peak_indices, detected_peak_values)
+
+def plot_data(axis, data, title='', fontsize=10):
+    axis.set_title(title, fontsize=fontsize)
+    axis.grid(which='both', axis='both', linestyle='--')
+    axis.plot(data, color="salmon", zorder=1)
+
+def plot_points(axis, values, indices):
+    axis.scatter(x=indices, y=values[indices], c="black", s=50, zorder=2)
+
+plt.close('all')
+fig, axarr = plt.subplots(1, sharex=True, figsize=(15, 18))
+
+plot_data(axis=axarr[0], data=MLII[:10000], title='Raw ECG measurements with QRS peaks marked (black)')
+plot_points(axis=axarr[0], values=ecg_data_detected, indices=qrs_peaks_indices)
+
+plt.show()
+plt.close()
+    
+
+
+plt.plot(MLII[:10000], '-gD',markevery=MLII[qrs_peaks_indices[:10]])
+plt.show()
+
+xs = np.linspace(-np.pi, np.pi, 30)
+ys = np.sin(xs)
+markers_on = [12, 17, 18, 19]
+plt.plot(xs, ys, '-gD', markevery=markers_on)
+plt.show()
+
+vals,poly = range(-60,60), range(-60,60)
+
+plt.plot(vals, poly, label='some graph')
+roots = [-1,1,2]
+
+mark = [vals.index(i) for i in roots]
+
+plt.plot(roots,[poly[i] for i in mark], ls="", marker="o", label="points")
+
+plt.show()
